1d280aa4d7b5efaaedc1d382163d0b0ec8b0e075
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-10-07 10:41:05 +0200

    added useOuterClick

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -29,3 +29,22 @@ export const useWindowDimensions = () => {
 
   return windowDimensions
 }
+
+export const useOuterClick = (onOuterClick, innerRef) => {
+  useEffect(
+    () => {
+      // only add listener, if the element exists
+      if (innerRef.current) {
+        document.addEventListener("click", handleClick)
+      }
+
+      // unmount previous first in case inputs have changed
+      return () => document.removeEventListener("click", handleClick)
+
+      function handleClick(e) {
+        innerRef.current && !innerRef.current.contains(e.target) && onOuterClick(e)
+      }
+    },
+    [onOuterClick, innerRef] // invoke again, if inputs have changed
+  )
+}
