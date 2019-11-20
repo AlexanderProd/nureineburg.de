de27966856ca58380b7850f0b294cfe757c517c7
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-11-20 18:44:29 +0100

    added Title and MainContent component

--- src/templates/Page/index.js
+++ src/templates/Page/index.js
@@ -1,16 +1,23 @@
 import React from 'react'
 
-import SEO from '../../components/seo'
-import Navigation from '../../components/Navigation'
-import { Container } from '../../utils/styles'
-import Footer from '../../components/Footer'
+import SEO from '~/components/seo'
+import Navigation from '~/components/Navigation'
+import { 
+  Container,
+  MainContent
+} from '~/utils/styles'
+import Footer from '~/components/Footer'
+import { Title } from './styles'
 
 const Page = ({ title, children }) => (
   <>
     <SEO title={title} />
     <Navigation />
     <Container>
-      {children}
+      <MainContent>
+        <Title>{title}</Title>
+        {children}
+      </MainContent>
       <Footer />
     </Container>
   </>
---
+++ src/templates/Page/styles.js
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled'
+
+export const Title = styled.h1`
+  font-size: 1.875rem;
+  font-family: "Old Standard TT",serif;
+  font-weight: 400;
+  margin: 0 0 0.5em;
+  line-height: 1.4;
+  text-align: center;
+  margin-bottom: 50px;
+
+  :after {
+    border-top: solid #1c1d1d;
+    border-width: 1px 0 0 0;
+    width: 50px;
+  }
+`
