52ea039c920cb9bdbc780d182ca20c88d08a94b8
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-11-20 18:40:45 +0100

    renamed page links, disabled favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,11 +18,11 @@ module.exports = {
     primaryNav: [
       {
         name: 'Produkte',
-        link: '/products'
+        link: '/produkte'
       },
       {
         name: 'Über Uns',
-        link: '/page-2',
+        link: '/ueber-uns',
       },
       {
         name: 'Kontakt',
@@ -85,7 +85,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
